c555be41414534fcb0d8818fbfa7002feb3af9ad
Jeffrey Sun <jsun624@gmail.com>
2020-04-19 02:20:19 -0700

    Player and enemy movement work

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+var players = {};
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+
+    socket.emit('currentPlayers', players);
+
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('disconnect', function () {
+        console.log('a user disconnected');
+
+        delete players[socket.id];
+
+        io.emit('disconnect', socket.id);
+    });
+
+    socket.on('playerMovement', function (movementData) {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        players[socket.id].rotation = movementData.rotation;
+
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
 });
 
 server.listen(8081, function () {
